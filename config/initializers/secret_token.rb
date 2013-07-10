a6b180e289ebc12a58813fb25055a4bb860fbb92
MakerSquare Student <students@themakersquare.com>
2013-07-10 18:19:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifatweetonrails::Application.config.secret_token = 'd6ce3ea293ea6165892f69f4df6cc7871fcda052ad2322cdab39de470ba2f2057915659f786caf9b859cb9c6a4a5a59f7ce99ad8db81c21fefbd325a299a8a34'
