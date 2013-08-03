4d9f4073df8701f791d58b1b415b733369cec2ba
Shannon Byrne <s.byrne2254@gmail.com>
2013-08-03 13:53:50 -0500

    Update twitter.rb
    Deleted keys before making repo public

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = "V7CKuaAEtaxEEuyQlcgrg"
-  config.consumer_secret = "w17O6llfGKaGSyLgAimWxd5eagtjanAtTiKbSseQo8"
-  config.oauth_token = "14168536-jrxLECbf1bRz4637GtCl6WGSuJGveFMSMJUF4GnZM"
-  config.oauth_token_secret = "LYnt14nCGqkh8LpcFLn3MguDC6SaSVw0NhLLwr5A"
+  config.consumer_key = ""
+  config.consumer_secret = ""
+  config.oauth_token = ""
+  config.oauth_token_secret = ""
 end
